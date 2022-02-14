59975cab09f7ecb88ea97cdd83cd0aa39eb7ea99
josfredus <59462599+josfredus@users.noreply.github.com>
2022-02-14 01:56:10 +0100

    Add files via upload

--- main.mjs
+++ main.mjs
@@ -1,111 +1,62 @@
-import {EventDirector} from './lib/EventDirector.mjs'
+import * as Outcome from './lib/types/Outcome.mjs'
 
-(async () => {
+import {ConfigScreen} from './lib/ConfigScreen.mjs'
+import {ProgressBar} from './lib/ProgressBar.mjs'
+import {SubredditValidator} from './lib/SubredditValidator.mjs'
+import {ExhibitionDirector} from './lib/ExhibitionDirector.mjs'
 
-const events = new EventDirector([
-  'entry-submit',
-  'entry-input',
-  'loading-success',
-  'loading-error',
-  'terminate-exhibition',
-])
+;(async () => {
 
-events.connectEvent({
-  destinationEvent: 'entry-submit',
-  baseEventTarget: window,
-  baseEventName: 'keydown',
-  conditionnal: event => ['a', 'A'].includes(event.key),
+await new Promise(r => document.addEventListener('DOMContentLoaded', r))
+
+const listenForEscape = () => new Promise(r => {
+  const callback = event => {
+    if (event.key === 'Escape' || event.key === 'Backspace') {
+      window.removeEventListener('keydown', callback)
+      r({validating: Outcome.Interrupted()})
+    }
+  }
+  window.addEventListener('keydown', callback)
 })
 
-events.connectEvent({
-  destinationEvent: 'entry-input',
-  baseEventTarget: window,
-  baseEventName: 'keydown',
-  conditionnal: event => ['z', 'Z'].includes(event.key),
-})
-
-events.connectEvent({
-  destinationEvent: 'loading-success',
-  baseEventTarget: window,
-  baseEventName: 'keydown',
-  conditionnal: event => ['e', 'E'].includes(event.key),
-})
-
-events.connectEvent({
-  destinationEvent: 'loading-error',
-  baseEventTarget: window,
-  baseEventName: 'keydown',
-  conditionnal: event => ['r', 'R'].includes(event.key),
-})
-
-events.connectEvent({
-  destinationEvent: 'terminate-exhibition',
-  baseEventTarget: window,
-  baseEventName: 'keydown',
-  conditionnal: event => ['t', 'T'].includes(event.key),
-})
+const configScreen = new ConfigScreen()
+const progressBar = new ProgressBar()
+const validator = new SubredditValidator()
+const exhibitionViewport = document.getElementById('exhibition-viewport')
+document.body.removeChild(exhibitionViewport)
 
 while (true) {
-  await events.waitFor({relevantEvents: ['entry-submit']})
-  console.log('entry submitted')
-  switch (
-    await events.waitFor({relevantEvents: [
-      'entry-input',
-      'loading-success',
-      'loading-error',
-    ]})
-  ) {
-  case 'entry-input':
-    console.log('input detected before loading completion')
-    break;
-  case 'loading-success':
-    console.log('loaded successfully')
-    await events.waitFor({relevantEvents: ['terminate-exhibition']})
-    console.log('asked to go back to entry state')
-    break;
-  case 'loading-error':
-    console.log('failed to load')
-    break;
+  const config = await configScreen.getConfig()
+  const {validating, barrenSubreddits} = await Promise.race([
+    validator.validate({
+      listsOfSortings: config.listsOfSortings,
+      progressBar,
+    }),
+    listenForEscape(),
+  ])
+  if (validating.isSuccess()) {
+    configScreen.hide()
+    configScreen.notifyBarrenSubreddits(barrenSubreddits)
+    document.body.appendChild(exhibitionViewport)
+    const director = new ExhibitionDirector({
+      listsOfSortings: validating.result(),
+      scraperStash: validator.getScraperStash(),
+      config,
+      progressBar,
+      viewport: exhibitionViewport,
+    })
+    director.run()
+    await listenForEscape()
+    director.terminate()
+    document.body.removeChild(exhibitionViewport)
+    configScreen.show()
+  } else if (validating.isFailure()) {
+    configScreen.notifyBarrenSubreddits(barrenSubreddits)
+    progressBar.hide()
+  } else if (validating.isInterrupted()) {
+    validator.cancel()
+    progressBar.hide()
   }
 }
 
-events.disconnectEvents()
-
 })()
-
-/*
-
-main algorithm:
-build up entry ui
-while true
-  wait for entry
-  start progress indicator (make it faux-finite with request timeout limit max)
-  create loaders for entry
-  wait for first media to be loadable or input on entry
-  if input on entry occurs before loading ends
-    cancel loaders
-    do cancel animation for loading progress indicator and terminate it
-    loop over
-  if we have a first media to load
-    terminate entry ui (remember entry line)
-    do success animation for loading progress indicator
-    build up exhibition ui
-    launch exhibition (morph loading progress indicator into timer indicator)
-    wait for go-back signal (it's not the exhibition's concern to detect that)
-    terminate exhibition and exhibition ui
-    build up entry ui (recover entry line)
-  otherwise
-    do failure animation for loading progress indicator and terminate it
-    notify failure to user in ui
-  loop over
-
-application flow:
-start -> entry state -A-> loading state -B-> exhibition state
-.         <--------------C-------
-.         <----------------------------D-------------
-A: entry entered from entry ui
-B: first media succesfully loaded
-C: entry changed in entry ui (not necessarily entered)
-D: go-back event (backspace or escape key for example)
-
-*/
